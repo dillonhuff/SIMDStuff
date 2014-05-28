e3accdbf5ce995ba16a8dc5d97aa76f118948c4c
Dillon Huff <dillonhuff@gmail.com>
2014-05-28 16:30:28 -0700

    having alignment issues

--- MMulSIMD.c
+++ MMulSIMD.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <smmintrin.h>
@@ -22,19 +23,30 @@ inline void mmmul_2x2(double *a, double *b, double *c) {
   __m128d a_00, a_01, a_10, a_11;
   __m128d b_00_01, b_10_11;
 
-  c_00_01 = _mm_load_pd(&c[0]);
-  c_10_11 = _mm_load_pd(&c[2]);
+  printf("two by two\n");
+
+  c_00_01 = _mm_loadu_pd(&c[0]);
+
+  printf("noooo\n");
+
+  c_10_11 = _mm_loadu_pd(&c[2]);
+
+  printf("load c\n");
 
   a_00 = _mm_loaddup_pd(&a[0]);
   a_01 = _mm_loaddup_pd(&a[1]);
   a_10 = _mm_loaddup_pd(&a[2]);
   a_11 = _mm_loaddup_pd(&a[3]);
 
+  printf("load a\n");
+
   b_00_01 = _mm_load_pd(&b[0]);
   b_10_11 = _mm_load_pd(&b[2]);
 
   c_00_01 += a_00 * b_00_01;
   c_10_11 += a_10 * b_00_01;
+
+  printf("penguins\n");
 
   c_00_01 += a_01 * b_10_11;
   c_10_11 += a_11 * b_10_11;
@@ -44,36 +56,81 @@ inline void mmmul_2x2(double *a, double *b, double *c) {
 }
 
 inline void mmmul_4x4(double *a, double *b, double *c) {
+  printf ("starting\n");
+
   mmmul_2x2(&a[0], &b[0], &c[0]);
+
+  printf("number 2\n");
+
   mmmul_2x2(&a[4], &b[8], &c[0]);
+
+  printf("done 0\n");
 
   mmmul_2x2(&a[0], &b[4], &c[4]);
   mmmul_2x2(&a[4], &b[12], &c[4]);
 
+  printf("done 1\n");
+
   mmmul_2x2(&a[8], &b[0], &c[8]);
   mmmul_2x2(&a[12], &b[8], &c[8]);
+
+  printf("done 2\n");
 
   mmmul_2x2(&a[8], &b[4], &c[12]);
   mmmul_2x2(&a[12], &b[12], &c[12]);
 }
 
-void rand_doubles(double *rands, int size) {
+void rand_doubles(int size, double *rands) {
   int i;
   for (i = 0; i < size; i++) {
-    rands[i] = rand();
+    rands[i] = rand() % 10;
   }
 }
 
+void simple_mmmul(int n, double *a, double *b, double *c)  {
+  int i, j, k;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        c[i*n + j] += a[i*n + k] * b[k*n + j];
+      }
+    }
+  }
+}
 
+double diff_buffer(int size, double *buf1, double *buf2)  {
+  int i;
+  double diff = 0.0;
+  for (i = 0; i < size; i++)  {
+    diff += abs(buf1[i] - buf2[i]);
+  }
+  return diff;
+}
+
+void copy_buffer(int size, double *src, double *dest) {
+  int i;
+  for (i = 0; i < size; i++) {
+    dest[i] = src[i];
+  }
+}
 
 int main()  {  
   double a[16];
   double b[16];
   double c[16];
+  double other_c[16];
 
-  rand_doubles(a, 16);
-  rand_doubles(b, 16);
-  rand_doubles(c, 16);
+  rand_doubles(16, a);
+  rand_doubles(16, b);
+  rand_doubles(16, c);
+
+  copy_buffer(16, c, other_c);
+
+  mmmul_4x4(a, b, c);
+  //simple_mmmul(4, a, b, other_c);
+
+  double diff = diff_buffer(16, c, other_c);
+  printf("diff = %f\n", diff);
 
   return 0;
 }
