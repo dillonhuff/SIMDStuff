9346e9a6a892204e29fe0cea383b0d6fa070153d
Dillon Huff <dillonhuff@gmail.com>
2014-05-28 16:58:29 -0700

    alignment of test case appears fixed

--- MMulSIMD.c
+++ MMulSIMD.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <smmintrin.h>
@@ -23,30 +24,19 @@ inline void mmmul_2x2(double *a, double *b, double *c) {
   __m128d a_00, a_01, a_10, a_11;
   __m128d b_00_01, b_10_11;
 
-  printf("two by two\n");
-
   c_00_01 = _mm_load_pd(&c[0]);
-
-  printf("noooo\n");
-
   c_10_11 = _mm_load_pd(&c[2]);
-
-  printf("load c\n");
 
   a_00 = _mm_loaddup_pd(&a[0]);
   a_01 = _mm_loaddup_pd(&a[1]);
   a_10 = _mm_loaddup_pd(&a[2]);
   a_11 = _mm_loaddup_pd(&a[3]);
 
-  printf("load a\n");
-
   b_00_01 = _mm_load_pd(&b[0]);
   b_10_11 = _mm_load_pd(&b[2]);
 
   c_00_01 += a_00 * b_00_01;
   c_10_11 += a_10 * b_00_01;
-
-  printf("penguins\n");
 
   c_00_01 += a_01 * b_10_11;
   c_10_11 += a_11 * b_10_11;
@@ -59,22 +49,13 @@ inline void mmmul_4x4(double *a, double *b, double *c) {
   printf ("starting\n");
 
   mmmul_2x2(&a[0], &b[0], &c[0]);
-
-  printf("number 2\n");
-
   mmmul_2x2(&a[4], &b[8], &c[0]);
-
-  printf("done 0\n");
 
   mmmul_2x2(&a[0], &b[4], &c[4]);
   mmmul_2x2(&a[4], &b[12], &c[4]);
 
-  printf("done 1\n");
-
   mmmul_2x2(&a[8], &b[0], &c[8]);
   mmmul_2x2(&a[12], &b[8], &c[8]);
-
-  printf("done 2\n");
 
   mmmul_2x2(&a[8], &b[4], &c[12]);
   mmmul_2x2(&a[12], &b[12], &c[12]);
@@ -114,11 +95,11 @@ void copy_buffer(int size, double *src, double *dest) {
   }
 }
 
-int main()  {  
-  double a[16];
-  double b[16];
-  double c[16];
-  double other_c[16];
+int main()  {
+  double *a = (double *) _aligned_malloc(16*sizeof(double), 16);
+  double *b = (double *) _aligned_malloc(16*sizeof(double), 16);
+  double *c = (double *) _aligned_malloc(16*sizeof(double), 16);
+  double *other_c = (double *) _aligned_malloc(16*sizeof(double), 16);
 
   rand_doubles(16, a);
   rand_doubles(16, b);
@@ -127,7 +108,7 @@ int main()  {
   copy_buffer(16, c, other_c);
 
   mmmul_4x4(a, b, c);
-  //simple_mmmul(4, a, b, other_c);
+  simple_mmmul(4, a, b, other_c);
 
   double diff = diff_buffer(16, c, other_c);
   printf("diff = %f\n", diff);
