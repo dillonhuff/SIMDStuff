bb6100fd6eaa08f36b1304154441ee34525a7b73
Dillon Huff <dillonhuff@gmail.com>
2014-05-29 12:00:49 -0700

    extracted some functions/includes into utils

--- DotProd.c
+++ DotProd.c
@@ -9,5 +9,11 @@ double simp_dp(int n, double *a, double *b, double alpha)	{
 }
 
 int main()	{
+	int n = 1000;
+	double *a = aligned_malloc_16(n*sizeof(double));
+	double *b = aligned_malloc_16(n*sizeof(double));
+	alpha = 0.0;
+	res = simp_dp(n, a, b, alpha);
+	printf("res = %f\n", res);
 	return 0;
 }
